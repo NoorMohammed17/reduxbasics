d2e4c91b25a6a26868d91c8bcee525438ca4b89d
NoorMohammed17 <noormuhammads1797@gmail.com>
2023-08-22 11:20:14 +0530

    toggle effect added

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,32 +1,40 @@
-import classes from './Counter.module.css';
-import {  useDispatch, useSelector } from 'react-redux';
+import classes from "./Counter.module.css";
+import { useDispatch, useSelector } from "react-redux";
 
 const Counter = () => {
-
-  const counter = useSelector( state => state.counter);
+  const counter = useSelector((state) => state.counter);
+  const show = useSelector((state) => state.showCounter);
   const dispatch = useDispatch();
 
   const incrementHandler = () => {
-    dispatch({type:'increment'})
-  }
+    dispatch({ type: "increment" });
+  };
   const increaseByValueHandler = () => {
-    dispatch({type:'increaseByValue', value:5} )
-  }
+    dispatch({ type: "increaseByValue", value: 5 });
+  };
 
   const decrementHandler = () => {
-    dispatch({type:'decrement'})
-  }
-  const toggleCounterHandler = () => {};
+    dispatch({ type: "decrement" });
+  };
+  const toggleCounterHandler = () => {
+    dispatch({ type: "toggle" });
+  };
+
+  const content = (
+    <div>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseByValueHandler}>IncreaseBy5</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
+    </div>
+  );
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
-      <div>
-        <button onClick={incrementHandler}>Increment</button>
-        <button  onClick={increaseByValueHandler}>IncreaseBy5</button>
-        <button onClick={decrementHandler}>Decrement</button>
-      </div>
+      {show && content}
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
--- src/store/store.js
+++ src/store/store.js
@@ -1,19 +1,32 @@
 import { createStore } from "redux";
+const initialState = {
+    counter :0,
+    showCounter : true
+}
 
-const counterReducer = (state={counter :0}, action) => {
+const counterReducer = (state=initialState, action) => {
     if(action.type === "increment"){
         return{
             counter: state.counter + 1,
+            showCounter:state.showCounter,
         }
     }
     if(action.type === "increaseByValue"){
         return{
             counter: state.counter + action.value,
+            showCounter:state.showCounter,
         }
     }
     if(action.type === "decrement"){
         return{
             counter: state.counter - 1,
+            showCounter:state.showCounter,
+        }
+    }
+    if(action.type === "toggle"){
+        return{
+            counter: state.counter ,
+            showCounter:!state.showCounter,
         }
     }
     return state;
